dd563bdc62d6cb578447e3feed6374ad9de4e446
luizcarlosom <luizcarlosoliveiramaciel@hotmail.com>
2024-08-15 10:21:52 -0300

    feat: Projeto utilizando arq MVC para estudo finalizado

--- src/controllers/people_register_controller.py
+++ src/controllers/people_register_controller.py
@@ -1,10 +1,12 @@
 from typing import Dict
+from src.models.repository.person_repository import person_repository
+from src.models.entities.person import Person
 
 class PeopleRegisterController:
     def register(self, new_person_informations: Dict) -> Dict:
         try:
             self.__validate_fields(new_person_informations)
-
+            self.__create_person_entity_and_store(new_person_informations)
             response = self.__format_response(new_person_informations)
             return { "success": True, "message": response }
         except Exception as exception:
@@ -19,6 +21,14 @@ class PeopleRegisterController:
 
         try: int(new_person_information["height"])
         except: raise Exception('Campo Altura Incorreto')
+
+    def __create_person_entity_and_store(self, new_person_informations: Dict) -> None:
+        name = new_person_informations["name"]
+        age = new_person_informations["age"]
+        height = new_person_informations["height"]
+
+        new_person = Person(name, age, height)
+        person_repository.registry_person(new_person)
 
     def __format_response(self, new_person_information: Dict) -> Dict:
         return {
---
+++ src/models/repository/person_repository.py
@@ -0,0 +1,16 @@
+from src.models.entities.person import Person
+
+class __PersonRepository:
+    def __init__(self) -> None:
+        self.__people = []
+
+    def registry_person(self, person: Person) -> None:
+        self.__people.append(person)
+
+    def find_person_by_name(self, name: str) -> Person:
+        for person in self.__people:
+            if person.name == name: return person
+        return None
+
+#Singleton
+person_repository = __PersonRepository()
